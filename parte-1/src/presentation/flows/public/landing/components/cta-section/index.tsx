4b84eabf4194aae0e6d060bdc5183d8633becd4a
euviniciuss <dev.mvsc@gmail.com>
2026-04-22 17:08:14 -0300

    feat: criando seção de cta

---
+++ parte-1/src/presentation/flows/public/landing/components/cta-section/index.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@components/ui/button";
+import { Typography } from "@components/ui/typography";
+import Image from "next/image";
+
+export function CTASection() {
+  return (
+    <section className="w-full">
+      <div className="relative w-full bg-gray rounded-[45px] p-[60px] flex">
+        <div className="max-w-[500px] flex flex-col gap-[26px]">
+          <Typography as="h3" variant="h3" className="text-black">
+            Let’s make things happen
+          </Typography>
+          
+          <Typography as="p" variant="p" className="text-black">
+            Contact us today to learn more about how our digital marketing services can help your business grow and succeed online.
+          </Typography>
+
+          <Button variant="primary">
+            Get your free proposal
+          </Button>
+        </div>
+
+      <div className="absolute top-1/2 -translate-y-1/2 right-[135px]">
+        <Image
+          src='/illustrations/cta.webp'
+          alt="Call to Action Illustration"
+          width={359} 
+          height={394.27}
+        />
+      </div>
+      </div>
+    </section>
+  );
+}
--- parte-1/src/presentation/flows/public/landing/index.tsx
+++ parte-1/src/presentation/flows/public/landing/index.tsx
@@ -1,7 +1,8 @@
-import { Header } from '@/presentation/components/ui/header';
+import { Header } from '@components/ui/header';
 import { CompanySection } from './components/company-section';
 import { HeroSection } from './components/hero-section';
 import { ServicesSection } from './components/services-section';
+import { CTASection } from './components/cta-section';
 
 export function LandingPage() {
   return (
@@ -10,6 +11,7 @@ export function LandingPage() {
       <HeroSection />
       <CompanySection />
       <ServicesSection />
+      <CTASection />
     </main>
   );
 }
