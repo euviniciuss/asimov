ee0154e3efe067fb5ac25f863b6a7202f74702f0
euviniciuss <dev.mvsc@gmail.com>
2026-04-24 02:22:02 -0300

    feat: ajustando import do button

--- parte-2/src/presentation/components/ui/header/index.tsx
+++ parte-2/src/presentation/components/ui/header/index.tsx
@@ -1,6 +1,6 @@
+import { Button } from "@components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "@components/ui/button";
 import { CustomLink } from "./components/custom-link";
 
 export function Header() {
@@ -32,4 +32,4 @@ export function Header() {
 			</div>
 		</nav>
 	);
-};
+}
--- parte-2/src/presentation/flows/public/landing/components/hero-section/index.tsx
+++ parte-2/src/presentation/flows/public/landing/components/hero-section/index.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import { Button } from "@components/ui/button";
+import Image from "next/image";
 import { FeatureCard } from "../feature-card";
 import { FEATURE_CARD_CONTENT } from "./constants";
 import { styles } from "./styles";
